bbcecb541f9913abe306e998b6cd39b71af4dc55
CaesarRulezzz <caesarrulezzz@gmail.com>
2026-03-28 14:01:50 +0300

    Unlock pro

--- app/src/play/java/eu/faircode/email/ActivityBilling.java
+++ app/src/play/java/eu/faircode/email/ActivityBilling.java
@@ -255,12 +255,13 @@ public class ActivityBilling extends ActivityBase implements
     }
 
     static boolean isPro(Context context) {
-        if (BuildConfig.DEBUG && false)
-            return true;
-        if (context == null)
-            return false;
-        return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean("pro", false);
+        return true;
+//        if (BuildConfig.DEBUG && false)
+//            return true;
+//        if (context == null)
+//            return false;
+//        return PreferenceManager.getDefaultSharedPreferences(context)
+//                .getBoolean("pro", false);
     }
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
